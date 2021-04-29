7f9305def2af06c3cbcc640c314ca3fc38b83d1a
Conor Ward <sirconorjamesward@gmail.com>
2021-04-28 19:01:34 -0600

    1.2

--- part 1/courseinfo/src/App.js
+++ part 1/courseinfo/src/App.js
@@ -6,19 +6,33 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+
+  const Header = ({course}) =>{
+    return(
+      <h1>{course}</h1>
+    );
+  }
+  const Part = ({part, exercise}) =>{
+    return (
+      <p>{part}{exercise}</p>
+    )
+  }
+  const Total = ({exercises}) =>{
+    let sum = 0
+    for(let i = 0; i < exercises.length; i++){
+      sum += exercises[i]
+    }
+    return(
+      <p>Number of exercises {sum}</p>
+    )
+  }
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1}{exercises1}
-      </p>
-      <p>
-        {part2}{exercises2}
-      </p>
-      <p>
-        {part3}{exercises3}
-      </p>
-      <p>Number of exercises {exercises1+exercises2+exercises3}</p>
+      <Header course={course}/>
+      <Part part={part1} exercise={exercises1}/>
+      <Part part={part2} exercise={exercises2}/>
+      <Part part={part3} exercise={exercises3}/>
+      <Total exercises={[exercises1,exercises2,exercises3]}/>
     </div>
   );
 }
