b0e9f86bffcb879125ed3a93a2c75ee708605dc2
Conor Ward <sirconorjamesward@gmail.com>
2021-04-29 08:26:18 -0600

    1.5

--- part 1/courseinfo/src/App.js
+++ part 1/courseinfo/src/App.js
@@ -1,33 +1,40 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React ',
-    exercises: 10
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-  const part2 = {
-    name: 'Using props to pass data ',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component ',
-    exercises: 14
-  }
-
   const Header = ({course}) =>{
     return(
-      <h1>{course}</h1>
+      <h1>{course.name}</h1>
     );
   }
-  const Part = ({part}) =>{
+  const Parts = ({content}) =>{
+    let partsList = []
+    for(let i = 0; i < content.parts.length; i++) {
+      console.log(content.parts[i].name, content.parts[i].exercises)
+      partsList.push(<p>{content.parts[i].name}, {content.parts[i].exercises}</p>)
+    }
     return (
-      <p>{part.name}{part.exercises}</p>
+      partsList
     )
   }
-  const Total = ({exercises}) =>{
-    console.log(exercises)
+  const Total = ({total}) =>{
     let sum = 0
-    for(let i = 0; i < exercises.length; i++){
-      sum += exercises[i]
+    for(let i = 0; i < total.parts.length; i++){
+      sum += total.parts[i].exercises
     }
     return(
       <p>Number of exercises {sum}</p>
@@ -36,10 +43,8 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Part part={part1}/>
-      <Part part={part2}/>
-      <Part part={part3}/>
-      <Total exercises={[part1.exercises, part2.exercises, part3.exercises]}/>
+      <Parts content={course}/>
+      <Total total={course}/>
     </div>
   );
 }
