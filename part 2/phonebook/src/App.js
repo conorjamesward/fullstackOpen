1573cb49c3ed14edf76a2456bdb0e86dc00ac22c
Conor Ward <sirconorjamesward@gmail.com>
2021-05-21 13:59:53 -0600

    2.11

--- part 2/phonebook/src/App.js
+++ part 2/phonebook/src/App.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect} from 'react'
 import FilterSearch from './components/FilterSearch'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
+
+useEffect(()=>{
+  axios.get('http://localhost:3001/phonebook')
+  .then(response=> {
+    setPersons(response.data)
+  })
+}, [])
   const handleNewPerson = (event) =>{
     event.preventDefault()
     if (persons.some(entry => entry.name === newName)){
--- part 2/phonebook/src/components/Person.js
+++ part 2/phonebook/src/components/Person.js
@@ -1,5 +1,13 @@
 import React from 'react'
 const Person = ({id, person}) => {
+  /*
+  wierd error where it says the id isn't unique, but it is...
+  I thought the component might be re-rendering created duplicates,
+  but that doesn't seem to be the case?
+  Because you can't add duplicate names it should be impossible to
+  have redundant keys, I haven't been able to figure out what is
+  wrong
+  */
   console.log('keys:', id)
   return(
     <li key={id}>
